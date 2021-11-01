0a1e1c7853adba6f18f5ddb1b072bf24cdab13ca
nggugu <nggudan@gmail.com>
2021-11-01 10:16:25 +0100

    Added tests for DFT and IDFT

--- lecture02/assignement01/exesrc/test07.c
+++ lecture02/assignement01/exesrc/test07.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <cmplx.h>
 
+// Code to test complex discrete fourier transform
+
 int main(void) {
+    printf("Code to test complex discrete fourier transform.\n");
+    printf("Test signal: f[n]={3,1,6,2}\n");
+
+    int N = 4;
+    cmplx_t input[4] = {{3.0,0.0}, {1.0,0.0}, {6.0,0.0}, {2.0,0.0}};
+    cmplx_t output[4];
+
+    cmplx_dft(&input, &output, N);
+
+    printf("Expected result:\n");
+    printf("\tF_0 = 12.00 + 0.00i\n");
+    printf("\tF_1 = -3.00 + 1.00i\n");
+    printf("\tF_2 = 6.00 + 0.00i\n");
+    printf("\tF_3 = -3.00 - 1.00i\n");
+    
+    printf("Calculated result:\n");
+    for(int i = 0; i < N; i++) {
+        printf("\tF_%d = %.2lf + %.2lfi\n", i,output[i][0], output[i][1]);
+    }
 
     return 0;
 }
--- lecture02/assignement01/exesrc/test08.c
+++ lecture02/assignement01/exesrc/test08.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <cmplx.h>
 
+// Code to test complex inverse discrete fourier transform
+
 int main(void) {
+    printf("Code to test complex inverse discrete fourier transform.\n");
+    printf("Test signal: f[n]={12.00 + 0.00i,-3.00 + 1.00i,6.00 + 0.00i,-3.00 - 1.00i}\n");
+
+    int N = 4;
+    cmplx_t input[4] = {{12.0,0.0}, {-3.0,1.0}, {6.0,0.0}, {-3.0,-1.0}};
+    cmplx_t output[4];
+
+    cmplx_idft(&input, &output, N);
+
+    printf("Expected result:\n");
+    printf("\tf(0) = 3.00 + 0.00i\n");
+    printf("\tf(1) = 1.00 + 0.00i\n");
+    printf("\tf(2) = 6.00 + 0.00i\n");
+    printf("\tf(3) = 2.00 + 0.00i\n");
+    
+    printf("Calculated result:\n");
+    for(int i = 0; i < N; i++) {
+        printf("\tf_%d = %.2lf + %.2lfi\n", i,output[i][0], output[i][1]);
+    }
 
     return 0;
 }
